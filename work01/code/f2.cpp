e5ee74f924146d21ea9abf207726bee22f3c9eab
Detect-er <56780551+Detect-er@users.noreply.github.com>
2020-03-15 10:46:07 +0800

    Add files via upload

---
+++ work01/code/f2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "function.h"
+using namespace std;
+
+void print_hello(){
+    cout<<"hello,pursuit!";
+}
---
+++ work01/code/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "function.h"
+using namespace std;
+
+int main(){
+    print_hello();
+    cout<<endl;
+    cout<<"The factorial of 5 is"<<factorial(5)<<endl;
+    return 0;
+}
