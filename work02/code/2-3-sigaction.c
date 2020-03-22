f64360f4f4a6e586e6563ed65b37a327f0839298
Detect-er <56780551+Detect-er@users.noreply.github.com>
2020-03-22 22:42:51 +0800

    Update 2-3-sigaction.c

--- work02/code/2-3-sigaction.c
+++ work02/code/2-3-sigaction.c
@@ -19,21 +19,21 @@ int main() {
     act.sa_flags = 0;
     sigaction(SIGINT, &act, 0);  //信号捕捉函数，捕捉Ctrl+C
 
-    printf("unblock before,sleep 5s\n");
+    printf("block before,sleep 5s\n");
     sleep(5);
     //屏蔽SIGINT信号
     sigemptyset(&newmask);
     sigaddset(&newmask, SIGINT);//将SIGINT添加到newmask中
     sigprocmask(SIG_BLOCK, &newmask, &oldmask);//将newmask添加到信号屏蔽字中，保存之前的集合到oldmask
     
-    printf("unblock after,sleep 5s\n");
+    printf("block after,sleep 5s\n");
     sleep (5);//在休眠期，SIGINT信号会被阻塞 
 
     sigpending(&testmask);//把阻塞的信号放入testmask中
     if (sigismember(&testmask, SIGINT)) //判断testmask集合中是否又sigint信号
         printf("\nSIGINT has been ignored\n");
 
-    printf("Now unblock SIGINT\n");
+    printf("Now SIGINT is unblocked\n");
     sigprocmask(SIG_SETMASK, &oldmask, NULL);//将信号屏蔽字设置为oldmask集合
     //此处开始处理信号，调用信号处理函数
     printf("Process cannot come here if running correctly.");
